af416756e0659b159f79f5fcc46d9df5e73f655e
tiagombp <tiagombp@gmail.com>
2020-10-07 19:10:12 -0300

    acrescenta dados dos agregadores, grava relacao de acoes sem agregadores.

--- R/analise.R
+++ R/analise.R
@@ -13,18 +13,41 @@ ploa_raw <- readxl::read_excel("./dados/dados_originais/SOF_PLOA_2021_STN_ajusta
 orgaos <- readxl::read_excel("./dados/dados_originais/tabela_orgao_cofin.xlsx", skip = 5) %>%
   select(uo = `uo Código`,
          orgao = `orgao Código`,
-         orgaoReal = `Órgãos Poder Executivo 2020`) %>%
+         orgao_decreto = `Órgãos Poder Executivo 2020`) %>%
   mutate(
-    orgaoRealNome = stringr::str_sub(orgaoReal, 9),
-    orgaoReal     = stringr::str_sub(orgaoReal, 1, 5),
-    orgaoRealNome = ifelse(
-      orgaoRealNome == "abinete da Vice-Presidência da República",
+    orgao_decreto_nome = stringr::str_sub(orgao_decreto, 9),
+    orgao_decreto = stringr::str_sub(orgao_decreto, 1, 5),
+    orgao_decreto_nome = ifelse(
+      orgao_decreto_nome == "abinete da Vice-Presidência da República",
       "Gabinete da Vice-Presidência da República",
-      orgaoRealNome)
+      orgao_decreto_nome)
     )
 
-ploa <- ploa_raw %>%
-  left_join(orgaos)
+agrupadores <- readxl::read_excel("./dados/dados_originais/Novos Agregadores Min. Economia.xlsx") %>%
+  select(
+    acao = `Rótulos de Linha`,
+    #tituloacao = `Nome Ação Governo`,
+    agregador = `Agregadores Novos`
+  )
 
+
+# reúne informações na tabela ---------------------------------------------
+
+ploa <- ploa_raw %>%
+  left_join(orgaos) %>%
+  left_join(agrupadores, by = "acao")
+
+ploa_ME <- ploa %>%
+  filter(orgao_decreto == "25000")
+
+acoes_ME_sem_agregadores <- ploa_ME %>%
+  filter(is.na(agregador)) %>%
+  select(acao, tituloacao) %>%
+  unique()
+
+write.csv2(acoes_ME_sem_agregadores, 
+           file = "./dados/dados_intermediarios/acoes_ME_sem_agregadores.csv",
+           fileEncoding = "UTF-8"
+           )
 
 
