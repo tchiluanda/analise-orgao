de2fe607f019edf22598948124d465c03e3c789d
tiagombp <tiagombp@gmail.com>
2020-11-10 16:25:24 -0300

    construindo o script

--- web/vis.js
+++ web/vis.js
@@ -1,21 +1,64 @@
 const vis = {
 
-    params : null,
+    refs: {
 
-    init : null,
+        svg: "svg.vis",
+        cont: "div.vis-container",
+        data: "./dados/dados.csv"
+
+    },
+
+    sels: {
+
+        svg: d3.select(vis.refs.svg),
+        cont: d3.select(vis.refs.cont)
+
+    },
+
+    elems: {
+
+        svg:  document.querySelector(vis.refs.svg),
+        cont: document.querySelector(vis.refs.cont)
+
+    },
+
+    dims : {
+
+        h: null,
+        w: null
+
+    },
+
+    params : {
+
+
+    },
+
+    init : function() {
+
+        vis.f.get_size();
+
+    },
 
     f : {
+
+        get_size: function() {
+
+            vis.dims.w = +vis.sels.svg.style("width").slice(0, -2);
+            vis.dims.h = +vis.sels.svg.style("height").slice(0, -2);
+
+        },
 
         scales : {
 
             domain : null
 
-
-
         }
-
 
 
     }
 
 }
+
+vis.init();
+console.log(vis);
