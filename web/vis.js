e16a0fdbede4f8cc29d5040332179bbc89b43a1f
tiagombp <tiagombp@gmail.com>
2020-11-11 16:23:32 -0300

    desenvolvimento

--- web/vis.js
+++ web/vis.js
@@ -11,7 +11,8 @@ const vis = {
     sels: {
 
         svg: null,
-        cont: null
+        cont: null,
+        rects_acoes: null
 
     },
 
@@ -33,6 +34,8 @@ const vis = {
     data : {},
 
     params : {
+
+        modes : ["agregado", "detalhado"],
 
         variables : ["atu_total", "varia", "varia_pct"]
 
@@ -69,6 +72,7 @@ const vis = {
             let pos_vis_y = vis.elems.svg.getBoundingClientRect().y;
 
             vis.dims.h = win_h - pos_vis_y - vis.dims.margins;
+            // subtraio a margem para usar como margem
             vis.dims.w = +vis.sels.svg.style("width").slice(0, -2);
 
         },
@@ -91,45 +95,111 @@ const vis = {
 
         },
 
-        get_domain : function(data, variable) {
-
-            return d3.extent(data, d => +d[variable]);
-
+        update_positions : {
+            // para quando for disparado um resize
         }
     },
 
     draw: {
 
-        domains : {},
+        domains : {
 
-        ranges : {},
+            initialize : function() {
 
-        begin : function(data) {
+                vis.params.variables.forEach(variable => {
+                    vis.draw.domains[variable] = vis.draw.domains.get(vis.data, variable);
+                });
 
-            console.log(data.columns);
-            vis.data = data;
+            },
 
-            vis.params.variables.forEach(variable => {
-                vis.draw.domains[variable] = vis.f.get_domain(data, variable);
-            });
+            get : function(data, variable) {
+
+                return d3.extent(data, d => +d[variable]);
+    
+            },
 
         },
 
-        agregado : {
+        ranges : {
 
-            scales : {
+            update : function() {
+
+                vis.draw.ranges.x = [ vis.dims.margins, vis.dims.w - vis.dims.margins ];
+    
+                vis.draw.ranges.y = [ vis.dims.margins, vis.dims.h - vis.dims.margins ]
+    
+            },
+
+            x : null,
+            y : null,
+            w : null
+
+        },
+
+        scales : {
+
+            initialize : function(mode, dimension) {
+
+                vis.draw.scales[mode][dimension]
+                  .range(vis.draw.ranges[dimension])
+    
+            },
+
+            agregado : {
 
                 x: d3.scaleLinear(),
-    
+
                 y: d3.scaleLinear(),
 
                 w: d3.scaleLinear()
 
+            },
+
+            detalhado : {
+
+                x: d3.scaleLinear(),
+        
+                y: d3.scaleLinear(),
+
+                r: d3.scaleSqrt()
+
+            }
+
         },
 
-        detalhado : {},
+        bubbles : {
 
-        card : {}
+            add : function() {
+
+                let svg = vis.sels.svg;
+
+                svg.selectAll("rect")
+
+            }
+
+
+        },
+
+        begin : function(data) {
+
+            console.log(data.columns);
+
+            // saves data as a property to make it easier to access it elsewhere
+            vis.data = data;
+
+            // evaluates domains for selected variables
+            vis.draw.domains.initialize();
+
+            // updates ranges
+            vis.draw.ranges.update();
+
+            
+
+        },
+
+        card : {
+
+            // outro script, talvez?
 
         }
 
